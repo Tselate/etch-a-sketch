f57f7ff8d70500d3c0b1b6abde85a3b55ffba5b7
Tselate <Tselatedawit@outlook.com>
2021-02-20 12:52:44 -0700

    Almost done

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ let width = document.getElementById("inputWidth");
 let columns = parseInt(document.getElementById("inputHeight").value);
 let rows = parseInt(document.getElementById("inputWidth").value);
 
-
-
+// Furction makes table 
 
 function makeTable () {
     let columns = parseInt(document.getElementById("inputHeight").value);
     let rows = parseInt(document.getElementById("inputWidth").value);
     let tbody = document.createElement("tbody");
-
 
     for (let i = 0; i < rows; i++) {
         let r = document.createElement("tr");
@@ -22,15 +20,67 @@ function makeTable () {
         tbody.appendChild(r);
     }
     bigTable.appendChild(tbody);
-    console.log(bigTable);
-}   
+    
+} 
+
+//Make table on click command 
+document.getElementById("makeTable").addEventListener("click", makeTable);
 
 
-function wow () {
+let any = Math.floor(Math.random()*16777215).toString(16);
+
+// Function to change each box into a random color on click
+function rgb () {
     let columns = parseInt(document.getElementById("inputHeight").value);
     let rows = parseInt(document.getElementById("inputWidth").value);
-    console.log(rows);
-    console.log(columns);
+    let tbody = document.createElement("tbody");
+
+    for (let i = 0; i < rows; i++) {
+        let r = document.createElement("tr");
+        for (let j = 0; j < columns; j++) {
+            data = document.createElement("td");
+            
+            r.appendChild(data);  
+        }
+        tbody.appendChild(r);
+        
+    }
+    
+    let boxes = document.getElementsByTagName("td");
+    for (let k = 0; k < boxes.length; k++) {
+        boxes[k].addEventListener("click", function () {
+            boxes[k].style.background = "#" + any;
+        }
+        );
+    }
 }
 
-document.getElementById("makeTable").addEventListener("click", makeTable);
+
+// Function to make each clicked box to turn black 
+function black () {
+    let columns = parseInt(document.getElementById("inputHeight").value);
+    let rows = parseInt(document.getElementById("inputWidth").value);
+    let tbody = document.createElement("tbody");
+
+    for (let i = 0; i < rows; i++) {
+        let r = document.createElement("tr");
+        for (let j = 0; j < columns; j++) {
+            data = document.createElement("td");
+            
+            r.appendChild(data);  
+        }
+        tbody.appendChild(r);
+        
+    }
+    
+    let boxes = document.getElementsByTagName("td");
+    for (let k = 0; k < boxes.length; k++) {
+        boxes[k].addEventListener("click", function () {
+            boxes[k].style.background = "#000";
+        }
+        );
+    }
+}
+
+
+
